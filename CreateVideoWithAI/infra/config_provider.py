9c264f205986fa4e8bdd93efbc30495f02118e4e
Bahaa <bahaa.abozlf57@gmail.com>
2024-11-09 18:28:58 +0200

    Absolute Path
    Updated the Absolute Path

--- CreateVideoWithAI/infra/config_provider.py
+++ CreateVideoWithAI/infra/config_provider.py
@@ -3,5 +3,5 @@ import json
 class ConfigProvider:
     @staticmethod
     def load_config_json():
-        with open('C:\\Users\\user\\PycharmProjects\\CreateVideoWithAI\\config.json', 'r') as file:
+        with open('C:\\Users\\user\\Documents\\GitHub\\CreateAIVideosAutomation\\CreateVideoWithAI\\config.json', 'r') as file:
             return json.load(file)
