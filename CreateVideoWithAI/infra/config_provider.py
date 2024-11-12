0a1e1c838b36e69053bf9fa9b30210edf84b95de
NOTxMeliiodaaS <kotarou.gaia557@gmail.com>
2024-11-12 22:14:25 +0200

    Added a sub repo + load prompt function
    a

--- CreateVideoWithAI/infra/config_provider.py
+++ CreateVideoWithAI/infra/config_provider.py
@@ -13,3 +13,9 @@ class ConfigProvider:
         with open('C:\\Users\\user\\Documents\\GitHub\\CreateAIVideosAutomation\\'
                   'CreateVideoWithAI\\secret.json', 'r') as file:
             return json.load(file)
+
+    @staticmethod
+    def load_prompt_config_text():
+        with open('C:\\Users\\Bahaa\\Desktop\\AI_Projects\\CreateAIVideosAutomation\\'
+                  'CreateVideoWithAI\\src\\prompts\\automation_prompts\\prompts.txt', 'r') as file:
+            return json.load(file)
